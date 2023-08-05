bdcfe47894535bed39064f8ff9f9d322b08da55c
anastasia_lykova <anastasia.7@mail.ru>
2023-08-06 01:41:47 +0500

    get_vacancies_hh

---
+++ src/hh_api.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import os
+
+
+def get_vacancies_hh(employers_id: list, keyword: str, page=3, per_page=100):
+    """
+    На вход получает список id работодателей и ключевое слово для поиска.
+    Обращается к сайту HH, после получения ответа записывает полученные вакансии
+    в файл vacancies.json, в основной директории
+    :param employers_id: список id работодателей
+    :param keyword: ключевое слово
+    :param page: количество страниц
+    :param per_page:  количество вакансий на странице
+    :return:
+    """
+    url = "https://api.hh.ru/vacancies"
+    headers = {"User-Agent": "Anastasia Lykova"}
+    vacancies_dict = {}
+
+    for id in employers_id:
+        for number in range(0, page+1):
+            params = {'employer_id': id, "text": keyword, "area": 113, "page": number, "per_page": per_page}
+            response = requests.get(url, params=params, headers=headers)
+            response = response.content.decode()
+            json_hh = json.loads(response)
+            for vacancy in json_hh['items']:
+                vacancies_dict[vacancy['id']] = vacancy
+
+    with open(os.path.join("..", "vacancies.json"), 'w', encoding="utf-8") as file:
+        json.dump(vacancies_dict, file, ensure_ascii=False, indent=4)
